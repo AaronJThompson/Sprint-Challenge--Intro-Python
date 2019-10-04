4e2328be24e8718cda3bb7bc2266fe6d71b019ca
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-04 09:26:01 +0100

    Create list of people whose name starts with a letter between C and G

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -36,7 +36,7 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [h for h in humans if h.name[0] in list(map(chr, range(ord('C'), ord('G')+1)))]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
