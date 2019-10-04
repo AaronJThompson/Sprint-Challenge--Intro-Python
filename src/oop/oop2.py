1332d7bd89f7f393c98a435c15f3209fdc52f729
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-04 09:12:37 +0100

    Add default value for ground vehicle

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,7 +4,7 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
     def drive(self):
@@ -19,6 +19,8 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+
+
 
 # TODO
 
